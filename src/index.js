7dfcea2a4c24d2248a0f9c651ef3807c0cbc09c9
allinthemind66 <r.nilon92@gmail.com>
2018-04-02 08:51:20 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 // Code Goes Here
+Order.propTypes = {
+  cone: PropTypes.bool,
+  size: PropTypes.string,
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+  orderInfo: PropTypes.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+  }).isRequired
+};
